ab3fbd935d25d05b97f55b10d139bb3b19c515f9
66160375 <160494374+66160375@users.noreply.github.com>
2024-12-19 22:19:16 +0700

    php

--- php01.php
+++ php01.php
@@ -4,22 +4,52 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="mystyle01.css" />
         <body>
-        
             <?php
             $myvar = 2; 
             ?>
-            <div class="text-bg-primary p-3 text-center"><h1>สูตรคูณแม่ <?php echo $myvar; ?></h1></div>
-            <?php 
-            for ($i = 1 ; $i <= 12; $i++){
-                $result = $myvar * $i;
-                
-            echo "$myvar * $i  = $result <br>";
+            <div class="p-3 mb-2 bg-danger-subtle text-danger-emphasis text-center"><h1 >สูตรคูณแม่ <?php echo $myvar; ?></h1></div>
             
-            }
-            ?>
+            <div class="container text-center">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col">
+                <table class="table table-hover ">
+                    <tbody>
+                        <?php 
+                        for ($i = 1 ; $i <= 12; $i++){
+                            $result = $myvar * $i;
+                            if ($i%2 == 0) {
+                                echo "<tr class='table-danger '>";
+                                echo "<th scope='row'> $myvar </th>"; 
+                                echo "<td '> X </td>";
+                                echo "<td> $i </td>";
+                                echo "<td>  =  </td>";
+                                echo "<td>  $result  </td>";
+                                echo "</tr>";
+                            } else {
+                                echo "<tr >";
+                                echo "<th scope='row'> $myvar </th>"; 
+                                echo "<td '> X </td>";
+                                echo "<td> $i </td>";
+                                echo "<td>  =  </td>";
+                                echo "<td>  $result  </td>";
+                                echo "</tr>";
+                            }
+                            }?>
+                    </tbody>
+                    </table>                   
+                </div>
+                <div class="col"></div>
+            </div>
+            </div>         
+
+
+
+            
         </body>
     </head>
 </html>
--- php02.php
+++ php02.php
@@ -4,21 +4,35 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <body>
-       
-            <h1>1-100 <?php ; ?></h1>
-            <?php 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="mystyle02.css" />    
+        <body >
+        <div class="p-3 text-primary-emphasis bg-primary-subtle border border-subtle-3 text-center">
+  <h1>เลขคู่ เลขคี่  <span class="badge text-bg-secondary">1-100</span></h1></div> 
+  <br><br>
+            <div class="container text-center">
+                <div class='row'>
+                        <div class="col"><div class="btn-group"> <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            เลขคู่ </button><ul class="dropdown-menu"><li>
+            <?php
             for ($i = 1 ; $i <= 100; $i++){
                 if ($i%2 == 0 ){
-                    echo "$i เป็นเลขคู่ <br>";
+                    echo "<a class='dropdown-item' href='#'> $i</a>";
                 }
-                else{
-                    echo "$i เป็นเลขคี่ <br>";
+            }?></li></ul></div></div> 
+            <div class="col"><div class="btn-group"> <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            เลขคี่ </button><ul class="dropdown-menu"><li>
+            <?php
+            for ($i = 1 ; $i <= 100; $i++){
+                if ($i%2 != 0 ){
+                    echo "<a class='dropdown-item' href='#'> $i</a>";
                 }
-            
-    
-            }
-            ?>
+            }?></li></ul></div></div>
+            </div>
+            </div>  <br><br> 
+            <img src="what.jpg" class="rounded float-start" alt="C:\xampp\htdocs\88823665-camp-66\what.jpg">        
+            <img src="whatToo.jpg" class="rounded float-end" alt="C:\xampp\htdocs\88823665-camp-66\whatToo.jpg">  
         </body>
     </head>
 </html>

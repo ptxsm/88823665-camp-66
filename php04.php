3ab05b7bae12d611fd55edb64b76fae9f4c2cc94
66160375 <160494374+66160375@users.noreply.github.com>
2024-12-23 21:41:47 +0700

    php

--- php04.php
+++ php04.php
@@ -1,38 +1,86 @@
 <?php
+
 /***/
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
+
+<head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="mystyle04.css" />    
-        <body >
-        <div class="p-3 text-primary-emphasis bg-primary-subtle border border-subtle-3 text-center">
-  <h1>เลขคู่ เลขคี่  <span class="badge text-bg-secondary">1-100</span></h1></div> 
-  <br><br>
-            <div class="container text-center">
-                <div class='row'>
-                        <div class="col"><div class="btn-group"> <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            เลขคู่ </button><ul class="dropdown-menu"><li>
-            <?php
-            for ($i = 1 ; $i <= 100; $i++){
-                if ($i%2 == 0 ){
-                    echo "<a class='dropdown-item' href='#'> $i</a>";
-                }
-            }?></li></ul></div></div> 
-            <div class="col"><div class="btn-group"> <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            เลขคี่ </button><ul class="dropdown-menu"><li>
-            <?php
-            for ($i = 1 ; $i <= 100; $i++){
-                if ($i%2 != 0 ){
-                    echo "<a class='dropdown-item' href='#'> $i</a>";
-                }
-            }?></li></ul></div></div>
+    <link rel="stylesheet" href="mystyle04.css" />
+
+<body class="container text-center">
+    <div class="p-3 text-primary-emphasis bg-primary-subtle border border-subtle-3 text-center">
+        <h1>เลขคู่ เลขคี่ <span class="badge text-bg-secondary">1-100</span></h1>
+    </div>
+    <br><br>
+    <div >
+        <div class='row'>
+            <div class="col"></div>
+            <div class="col">
+                <form method="POST" action="http://localhost/88823665-camp-66/php04.php?">
+                    <div class="form-floating"><input type="Number" class="form-control" id="floatingInput" placeholder="num" name="num">
+                        <label for="floatingInput">Input Number</label>
+                    </div>
             </div>
-            </div>  <br><br> 
-            <img src="what.jpg" class="rounded float-start" alt="C:\xampp\htdocs\88823665-camp-66\what.jpg">        
-            <img src="whatToo.jpg" class="rounded float-end" alt="C:\xampp\htdocs\88823665-camp-66\whatToo.jpg">  
-        </body>
-    </head>
+            <div class="col">
+                <form method="POST" action="http://localhost/88823665-camp-66/php04.php?">
+                    <div class="form-floating"><input type="Number" class="form-control" id="floatingInput" placeholder="num" name="num1">
+                        <label for="floatingInput">Input Number</label>
+                    </div>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class='row'>
+            <div class="col"></div>
+            <div class="col">
+                <div class="d-grid gap-2"><input class="btn btn-primary" type="submit" value="Submit"></div>
+                </form>
+            </div>
+            <div class="col"></div>
+        </div>
+    </div> <br><br>
+    <div class="row">
+                <div class="col"></div>
+                
+    <?php
+            if (isset($_POST['num']) && isset($_POST['num1'])) {
+                $myvar = $_POST['num'];
+                $myvar1 = $_POST['num1'];
+                if ($myvar < $myvar1) {
+                    $min = $myvar;
+                    $max = $myvar1;
+                } else {
+                    $min = $myvar1;
+                    $max = $myvar;
+                }
+    
+                echo "<div class='col'><h1>เลขคู่</h1>";
+                
+                for ($i = $min; $i <= $max; $i++) {
+                   
+                    if ($i % 2 == 0) {
+
+                        echo "<table class='table table-striped'><tr><td><div class='d-flex justify-content-center'> {$i}</div></td><tr></table>";
+                    } 
+                  
+                }
+                echo "</div>";
+                echo "<div class='col'><h1  >เลขคี่</h1>";
+                for ($i = $min; $i <= $max; $i++) {
+                   
+                    if ($i % 2 != 0) {
+                        echo "<table class='table table-striped'><tr><td><div class='d-flex justify-content-center'> {$i}</div></td><tr></table>";
+                    } 
+                  
+                }
+                echo "</div>";
+            }
+            ?>
+            
+            <div class="col"></div></div>
+</body>
+</head>
+
 </html>

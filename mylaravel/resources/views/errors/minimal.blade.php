d526943048e4e8516feb8499232205b8d4793192
66160375 <160494374+66160375@users.noreply.github.com>
2025-01-15 18:18:12 +0700

    laravel-template

---
+++ mylaravel/resources/views/errors/minimal.blade.php
@@ -0,0 +1,9 @@
+@extends ('layouts.default')
+
+@section('content')
+    <div class="login-page">    
+        @yield('code')
+        @yield('message')
+    </div>
+
+@endsection
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,16 +1,27 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/mycontroller/{id?}',
- [MyController::class, 'myfunction']);
+Route::get('/login}',
+ [LoginController::class, 'index']);
+
+Route::get('/register',
+ [RegisterController::class, 'index']);
+ Route::get('/home',
+ [HomeController::class, 'index']);
+ Route::get('/',
+ [HomeController::class, 'index']);
 
  Route::post('/mycontroller/{id?}',
  [MyController::class, 'myfunction']);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 

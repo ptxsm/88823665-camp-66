b235e0686e8a8d91576310fdbc31292959091d58
66160375 <160494374+66160375@users.noreply.github.com>
2025-02-13 15:46:03 +0700

    update login

--- mylaravel/resources/views/login.blade.php
+++ mylaravel/resources/views/login.blade.php
@@ -9,14 +9,18 @@
       <div class="card">
         <div class="card-body login-card-body">
           <p class="login-box-msg">Sign in to start your session</p>
-          <form action="../index3.html" method="post">
+          <?php
+            $error = session('error');
+            echo "<h3 class = 'text-danger'>".$error."</h3>";
+          ?>
+          <form action="{{url('/login')}}" method="post">
             @csrf
             <div class="input-group mb-3">
-              <input type="email" class="form-control" placeholder="Email" />
+              <input type="email" name="email" class="form-control" placeholder="Email" />
               <div class="input-group-text"><span class="bi bi-envelope"></span></div>
             </div>
             <div class="input-group mb-3">
-              <input type="password" class="form-control" placeholder="Password" />
+              <input type="password" name ="password"class="form-control" placeholder="Password" />
               <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
             </div>
             <!--begin::Row-->
@@ -24,7 +28,7 @@
               <div class="col-8">
                 <div class="form-check">
                   <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                  <label class="form-check-label" for="flexCheckDefault"> Remember Me </label>
+                  <label class="form-check-label " for="flexCheckDefault"> Remember Me </label>
                 </div>
               </div>
               <!-- /.col -->
@@ -41,7 +45,7 @@
 
           </div>
           <!-- /.social-auth-links -->
-          <p class="mb-1"><a href="forgot-password.html">I forgot my password</a></p>
+          <p class="mb-1 "><a href="forgot-password.html ">I forgot my password</a></p>
           <p class="mb-0">
             <a href="register.html" class="text-center"> Register a new membership </a>
           </p>
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -7,9 +7,26 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\CheckLogin;
+
+Route::middleware([CheckLogin::class])->group(function(){
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'edit']);
+Route::put('/user',[UserController::class ,'edit_action']);
+Route::delete('/user',[UserController::class, 'delete']);
+});
+Route::get('/',[
+    HomeController::class, 'index'])->middleware([CheckLogin::class]);
 
 Route::get('/login',
  [LoginController::class, 'index']);
+Route::post('/login',
+[LoginController::class, 'login']);
+Route::get('/logout',function(){
+    session()->forget('user');
+    session()->flush();
+    return redirect('/login');
+});
 
 Route::get('/register',
  [RegisterController::class, 'index']);
@@ -26,10 +43,6 @@ Route::get('/', function () {
     return view('home');    
 });
 Route::post('/register',[RegisterController::class, 'store']);
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/user/{id}', [UserController::class, 'edit']);
-Route::put('/user',[UserController::class ,'edit_action']);
-Route::delete('/user',[UserController::class, 'delete']);
 
 Route::get('/hello{/id?}', 
     function ($val = ""){
